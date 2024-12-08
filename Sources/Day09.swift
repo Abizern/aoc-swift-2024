37f2a830352880dd4b2867fcb1a6c99833ebc204
Abizer Nasir <abizern@mac.com>
2024-12-08 16:55:06 +0000

    Setup for Day9

---
+++ Sources/Day09.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct Day09: AdventDay, Sendable {
+  let data: String
+  let day = 9
+  let puzzleName: String = "--- Day 0: Placeholder! ---"
+
+  init(data: String) {
+    self.data = data
+  }
+
+  func part1() async throws -> Int {
+    0
+  }
+}
+
+extension Day09 {}
