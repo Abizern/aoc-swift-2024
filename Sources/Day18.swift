5147d5b695d13d2c53cbb04fe37b384ba42fd2d4
Abizer Nasir <abizern@mac.com>
2024-12-18 09:02:16 +0000

    Day 18 part 1

--- Sources/Day18.swift
+++ Sources/Day18.swift
@@ -1,7 +1,11 @@
+import AoCCommon
 import Foundation
+import GameplayKit
+
+typealias GridGraph = GKGridGraph<GKGridGraphNode>
+typealias Node = GKGridGraphNode
 
 struct Day18: AdventDay, Sendable {
-  // Save your data in a corresponding text file in the `Data` directory.
   let data: String
   let day = 18
   let puzzleName: String = "--- Day 0: Placeholder! ---"
@@ -10,11 +14,43 @@ struct Day18: AdventDay, Sendable {
     self.data = data
   }
 
-  // Replace this with your solution for the first part of the day's challenge.
+  var points: [(Int, Int)] {
+    do {
+      return try NumberPairs().parse(data)
+    } catch {
+      fatalError("Unable to parse input \(error)")
+    }
+  }
+
   func part1() async throws -> Int {
-    0
+    let graph = createGraph(width: 71, height: 71)
+    let points = Array(points.prefix(1024))
+    remove(points: points, from: graph)
+
+    let start = graph.node(atGridPosition: vector_int2(0, 0))!
+    let end = graph.node(atGridPosition: vector_int2(70, 70))!
+
+    let path = graph.findPath(from: start, to: end)
+
+    return path.count - 1 // steps, is one less than length of path
   }
 }
 
-// Add any extra code and types in here to separate it from the required behaviour
-extension Day18 {}
+extension Day18 {
+  func createGraph(width: Int, height: Int) -> GridGraph {
+    GKGridGraph(
+      fromGridStartingAt: vector_int2(0, 0),
+      width: Int32(width),
+      height: Int32(height),
+      diagonalsAllowed: false,
+      nodeClass: Node.self
+    )
+  }
+
+  func remove(points: [(Int, Int)], from graph: GridGraph) {
+    let nodes = points.map { vector_int2(Int32($0.1), Int32($0.0)) }
+      .compactMap { graph.node(atGridPosition: $0) }
+
+    graph.remove(nodes)
+  }
+}
--- Tests/Day18Tests.swift
+++ Tests/Day18Tests.swift
@@ -9,7 +9,7 @@ struct Day18Tests {
     @Test("Test parser implementation")
     func parseInput() {
       let day = Day18(data: testInput)
-
+      #expect(day.points.count == 25)
     }
   }
 
@@ -37,4 +37,29 @@ struct Day18Tests {
 
 private let testInput =
   """
+  5,4
+  4,2
+  4,5
+  3,0
+  2,1
+  6,3
+  2,4
+  1,5
+  0,6
+  3,3
+  2,6
+  5,1
+  1,2
+  5,5
+  2,5
+  6,5
+  1,4
+  0,4
+  6,4
+  1,1
+  6,1
+  1,0
+  0,5
+  1,6
+  2,0
   """
