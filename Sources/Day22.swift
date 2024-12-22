44ae8026ea0d7e2ebe282c5051f4be2a2e20902f
Abizer Nasir <abizern@mac.com>
2024-12-22 05:55:13 +0000

    Day22 Part 1

--- Sources/Day22.swift
+++ Sources/Day22.swift
@@ -1,20 +1,67 @@
 import Foundation
+import Parsing
 
 struct Day22: AdventDay, Sendable {
-  // Save your data in a corresponding text file in the `Data` directory.
   let data: String
   let day = 22
-  let puzzleName: String = "--- Day 0: Placeholder! ---"
+  let puzzleName: String = "--- Day 22: Monkey Market ---"
 
   init(data: String) {
     self.data = data
   }
 
-  // Replace this with your solution for the first part of the day's challenge.
+  var rows: [Int] {
+    do {
+      return try InputParser().parse(data)
+    } catch {
+      fatalError("Unable to parse data: \(error)")
+    }
+  }
+
   func part1() async throws -> Int {
-    0
+    rows.map(evolvutionFunction(n: 2000)).reduce(0, +)
   }
 }
 
-// Add any extra code and types in here to separate it from the required behaviour
-extension Day22 {}
+extension Day22 {
+  func mix(_ number: Int, _ secretNumber: Int) -> Int {
+    number ^ secretNumber
+  }
+
+  func prune(_ secretNumber: Int) -> Int {
+    secretNumber % 16777216
+  }
+
+  func next(from: Int) -> Int {
+    let a = prune(mix(from * 64, from))
+    let b = prune(mix(Int(floor(Double(a) / 32.0)), a))
+    let c = prune(mix(b * 2048, b))
+
+    return c
+  }
+
+  func evolvutionFunction(n: Int) -> (Int) -> Int {
+    { secretNumber in
+      var num = secretNumber
+      for _ in 0 ..< n {
+        num = next(from: num)
+      }
+
+      return num
+    }
+  }
+}
+
+extension Day22 {
+  struct InputParser: Parser {
+    var body: some Parser<Substring, [Int]> {
+      Many {
+        Int.parser()
+      } separator: {
+        "\n"
+      } terminator: {
+        End()
+      }
+    }
+  }
+}
--- Tests/Day22Tests.swift
+++ Tests/Day22Tests.swift
@@ -9,7 +9,7 @@ struct Day22Tests {
     @Test("Test parser implementation")
     func parseInput() {
       let day = Day22(data: testInput)
-
+      #expect(day.rows == [1, 10, 100, 2024])
     }
   }
 
@@ -17,12 +17,30 @@ struct Day22Tests {
   struct SolutionsTests {
     let day = Day22(data: testInput)
 
+    @Test("Next function")
+    func testNext() {
+      #expect(day.next(from: 123) == 15887950)
+      #expect(day.next(from: 15887950) == 16495136)
+      #expect(day.next(from: 16495136) == 527345)
+      #expect(day.next(from: 527345) == 704524)
+      #expect(day.next(from: 704524) == 1553684)
+      #expect(day.next(from: 1553684) == 12683156)
+      #expect(day.next(from: 12683156) == 11100544)
+      #expect(day.next(from: 11100544) == 12249484)
+      #expect(day.next(from: 12249484) == 7753432)
+      #expect(day.next(from: 7753432) == 5908254)
+    }
+
+    @Test("Evolution function")
+    func testEvolution() {
+      let fn = day.evolvutionFunction(n: 10)
+      #expect(fn(123) == 5908254)
+    }
+
     @Test("Part1 example")
     func testPart1() async throws {
-      await withKnownIssue {
-        let result = try await day.part1()
-        #expect(result == 10)
-      }
+      let result = try await day.part1()
+      #expect(result == 37327623)
     }
 
     @Test("Part2 example")
@@ -37,4 +55,8 @@ struct Day22Tests {
 
 private let testInput =
   """
+  1
+  10
+  100
+  2024
   """
