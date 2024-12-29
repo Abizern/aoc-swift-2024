445977cc647ca53a31e63defc2134a4b6a346d50
Abizer Nasir <abizern@mac.com>
2024-12-29 10:41:29 +0000

    Swiftformat

--- Sources/Day25.swift
+++ Sources/Day25.swift
@@ -21,8 +21,8 @@ struct Day25: AdventDay, Sendable {
   }
 
   func part1() async throws -> Int {
-    let keys = inputs.filter { if case .key = $0 { return true } else { return false } }
-    let locks = inputs.filter { if case .lock = $0 { return true} else { return false} }
+    let keys = inputs.filter { if case .key = $0 { true } else { false } }
+    let locks = inputs.filter { if case .lock = $0 { true } else { false } }
     var counts = 0
     for key in keys {
       for lock in locks {
@@ -40,7 +40,7 @@ extension Day25 {
     case lock([Int])
     case key([Int])
 
-    init (_ pins: [[Character]]) {
+    init(_ pins: [[Character]]) {
       let isLock = pins[0].contains("#")
       var heights: [Int] = Array(repeating: 0, count: 5)
       for col in 0 ..< 5 {
@@ -49,7 +49,7 @@ extension Day25 {
           pins[2][col],
           pins[3][col],
           pins[4][col],
-          pins[5][col]
+          pins[5][col],
         ].filter { $0 == "#" }
           .count
       }
@@ -61,8 +61,9 @@ extension Day25 {
     }
 
     func fits(_ key: FivePin) -> Bool {
-      guard case let .lock(lPins) = self,
-            case let .key(kPins) = key else {
+      guard case .lock(let lPins) = self,
+            case .key(let kPins) = key
+      else {
         return false
       }
 
@@ -75,7 +76,6 @@ extension Day25 {
     }
   }
 }
-
 
 extension Day25 {
   struct FivePinParser: Parser {
